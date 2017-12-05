0ff9fdc2f8868e3d06d308b50595c12dd9cbf2bb
hizhizhiz <hizhizhizhh@gmail.com>
2017-12-05 20:23:32 +1000

    1

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
-<?php 
-	echo "Let's start the project!";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Kisalala</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			
+		</div> 
+	</header>
 
-?>
+	<main>
+
+	</main>
+
+	<footer>
+
+	</footer>
+</body>
+</html>
